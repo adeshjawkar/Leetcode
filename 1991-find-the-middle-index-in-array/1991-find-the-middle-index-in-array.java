0ade2a6e794f43e577b11befd0a76a9cba375669
adeshjawkar <adeshj273@gmail.com>
2026-05-31 19:56:07 +0530

    Time: 0 ms (100%), Space: 43.6 MB (37.32%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -1,21 +1,35 @@
 class Solution {
     public int findMiddleIndex(int[] nums) {
         int n = nums.length;
+         //Bruetforce Approach
+        // for(int i=0; i<n; i++){
+        //     int left =0;
+        //     int right =0;
+        //     for(int j=0; j<i; j++){
+        //         left += nums[j];
+        //     }
+        //     for(int k=i+1; k<n; k++){
+        //         right += nums[k];
+        //     }
+        //     if(left==right){
+        //         return i;
+        //     }
+
+        // }
+        // return -1;
+        int totalSum =0;
         for(int i=0; i<n; i++){
-            int left =0; 
-            int right = 0;
-            for(int j=0; j<i; j++){
-                left += nums[j];
-            }
-            for(int k=i+1; k<n; k++){
-                right += nums[k];
-            }
-            if(left==right){
+            totalSum += nums[i];
+
+        }
+        int leftSum=0;
+        for(int i=0; i<n; i++){
+            int rightSum = totalSum-leftSum-nums[i];
+            if(leftSum==rightSum){
                 return i;
             }
+            leftSum += nums[i];
         }
         return -1;
-
-        
     }
 }
