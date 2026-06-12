dbb7dc7839ece25fe8fd8a5e0920dfbd32b4a673
adeshjawkar <adeshj273@gmail.com>
2026-06-12 16:59:13 +0530

    Time: 10 ms (26.5%), Space: 47.2 MB (13.92%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -20,10 +20,11 @@ class Solution {
         Arrays.sort(nums);
         int i=0;
         while(i<nums.length-1){
-            if(nums[i] != nums[i+1]) return nums[i];
-            i += 2;
+                if(nums[i] != nums[i+1])   return nums[i];
+                i += 2;
         }
         return nums[nums.length-1];
+
         
     }
 }
