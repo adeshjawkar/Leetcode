1793c9ff5cd142ee61440e62d40d39f768c52185
adeshjawkar <adeshj273@gmail.com>
2026-06-13 07:55:38 +0530

    Time: 536 ms (5.26%), Space: 47.1 MB (13.93%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,30 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        //method-1 Bruet force
-        // for(int i=0; i<nums.length; i++){
-        //     boolean flag = false;
-        //     for(int j=0; j<nums.length; j++){
-        //         if(i==j) continue;
-        //         if(nums[i]==nums[j]){
-        //             flag = true;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            boolean ans = false;
+            for(int j=0; j<n; j++){
+                if(i==j) continue;
+                if(nums[i]==nums[j]){
+                    ans = true;
+                }
 
-        //         }
-                
-        //     }
-        //     if(flag == false) return nums[i];
+            }
+         if(ans == false) return nums[i];
 
-        // }
-        // return 1;
-
-        //method-2 Using Sorting
-        Arrays.sort(nums);
-        int i=0;
-        while(i<nums.length-1){
-                if(nums[i] != nums[i+1])   return nums[i];
-                i += 2;
         }
-        return nums[nums.length-1];
-
-        
+        return 1;
     }
 }
